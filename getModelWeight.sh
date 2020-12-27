c68d5bbb82b73fa20129298a2ecb58bbf423e3b5
林毓宸 <linyuchen@MBA.local>
2020-12-27 10:44:18 +0800

    cifar commit

--- getModelWeight.sh
+++ getModelWeight.sh
@@ -1,9 +1,9 @@
 # CIFAR10
-!gdown -O inceptionv4_cifar10.pth --id 1lCJUYmdheeOyVkh6fU7NInioa7W6087Z
-!gdown -O resnet152_cifar10.pth --id 1FJYHQauf-IVVE5YzjQ5E8uZ4UxNp76K8
-!gdown -O vgg19_cifar10.pth --id 1QpFc7_L-cAqpfioRMJitExUd4SWEN78H
+gdown -O inceptionv4_cifar10.pth --id 1lCJUYmdheeOyVkh6fU7NInioa7W6087Z
+gdown -O resnet152_cifar10.pth --id 1FJYHQauf-IVVE5YzjQ5E8uZ4UxNp76K8
+gdown -O vgg19_cifar10.pth --id 1QpFc7_L-cAqpfioRMJitExUd4SWEN78H
 
 # CIFAR100
-!gdown -O inceptionv4_cifar100.pth --id 1q-klQg3WzQxd1sH2u388MregobTslZEy
-!gdown -O resnet152_cifar100.pth --id 1bR1Rw7ldm7GKe70Wh_HJrH2zujHJnfQQ
-!gdown -O vgg19_cifar100.pth --id 1tG-qgG-WYEESwVwx0aG0BeAviOQRzG-d
+gdown -O inceptionv4_cifar100.pth --id 1q-klQg3WzQxd1sH2u388MregobTslZEy
+gdown -O resnet152_cifar100.pth --id 1bR1Rw7ldm7GKe70Wh_HJrH2zujHJnfQQ
+gdown -O vgg19_cifar100.pth --id 1tG-qgG-WYEESwVwx0aG0BeAviOQRzG-d
